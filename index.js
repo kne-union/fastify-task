561773e40261fd8f01271f3003a6aefc505c9359
Linzp <sunandmoon001@163.com>
2025-07-30 14:16:53 +0800

    修改bug

--- index.js
+++ index.js
@@ -1,62 +1,69 @@
 const fp = require('fastify-plugin');
 const path = require('node:path');
 
-module.exports = fp(async (fastify, options) => {
-  options = Object.assign(
-    {},
-    {
-      dbTableNamePrefix: 't_',
-      prefix: '/api/task',
-      name: 'task',
-      limit: 10,
-      dir: path.resolve(process.cwd(), 'libs', 'tasks'),
-      syncCron: '*/10 * * * *',
-      scriptName: 'index',
-      maxPollTimes: 100,
-      pollInterval: 5000,
-      getUserModel: () => {
-        return fastify.account.user;
+module.exports = fp(
+  async (fastify, options) => {
+    options = Object.assign(
+      {},
+      {
+        dbTableNamePrefix: 't_',
+        prefix: '/api/task',
+        name: 'task',
+        limit: 10,
+        dir: path.resolve(process.cwd(), 'libs', 'tasks'),
+        syncCron: '*/10 * * * *',
+        scriptName: 'index',
+        maxPollTimes: 100,
+        pollInterval: 5000,
+        getUserModel: () => {
+          return fastify.account.models.user;
+        },
+        getAuthenticate: () => {
+          return [fastify.account.authenticate.user, fastify.account.authenticate.admin];
+        },
+        task: {}
       },
-      getAuthenticate: () => {
-        return [fastify.account.authenticate.user, fastify.account.authenticate.admin];
-      },
-      task: {}
-    },
-    options
-  );
+      options
+    );
 
-  fastify.register(require('@kne/fastify-namespace'), {
-    options,
-    name: options.name,
-    modules: [
-      ['controllers', path.resolve(__dirname, './libs/controllers')],
-      [
-        'models',
-        await fastify.sequelize.addModels(path.resolve(__dirname, './libs/models'), {
-          prefix: options.dbTableNamePrefix,
-          getUserModel: options.getUserModel,
-          modelPrefix: options.name
-        })
-      ],
-      ['services', path.resolve(__dirname, './libs/services')]
-    ]
-  });
+    fastify.register(require('@kne/fastify-namespace'), {
+      options,
+      name: options.name,
+      modules: [
+        ['controllers', path.resolve(__dirname, './libs/controllers')],
+        [
+          'models',
+          await fastify.sequelize.addModels(path.resolve(__dirname, './libs/models'), {
+            prefix: options.dbTableNamePrefix,
+            getUserModel: options.getUserModel,
+            modelPrefix: options.name
+          })
+        ],
+        ['services', path.resolve(__dirname, './libs/services')]
+      ]
+    });
 
-  fastify.register(
-    require('fastify-plugin')(async fastify => {
-      options.syncCron &&
-        fastify.register(require('fastify-cron'), {
-          jobs: [
-            {
-              cronTime: options.syncCron,
-              onTick: async () => {
-                const { runner } = fastify[options.name].services;
-                await runner();
-              },
-              start: true
-            }
-          ]
-        });
-    })
-  );
-});
+    fastify.register(
+      require('fastify-plugin')(async fastify => {
+        if (options.syncCron) {
+          fastify.cron.createJob({
+            cronTime: options.syncCron,
+            onTick: async () => {
+              const { runner } = fastify[options.name].services;
+              await runner();
+            },
+            start: true
+          });
+          //启动时，将running状态的任务设置为pending
+          fastify.addHook('onReady', async () => {
+            await fastify[options.name].services.resetAll();
+          });
+        }
+      })
+    );
+  },
+  {
+    name: 'fastify-task',
+    dependencies: ['fastify-cron']
+  }
+);
